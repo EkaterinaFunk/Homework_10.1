78d3b28565ed826d2bee5e5e9d2b1bdba660c8f1
екатерина функ <funkkatysha@gmail.com>
2024-05-25 23:29:05 +0700

    add second function

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 def changing_the_list(list_of_dict: list, state: str = "EXECUTED") -> list:
     """
     Функция, которая принимает на вход список словарей и значение для ключа state
@@ -9,3 +12,12 @@ def changing_the_list(list_of_dict: list, state: str = "EXECUTED") -> list:
         if state_key["state"] == state:
             default_list_dictionary.append(state_key)
     return default_list_dictionary
+
+
+def sort_data(operation: list[dict[str, Any]], new_list: bool = True) -> list[dict[str, Any]] | None:
+    """
+    Функция, которая принимает на вход список словарей и возвращает новый список,
+    в котором исходные словари отсортированы по убыванию даты
+    """
+    sorted_data: list | None = sorted(operation, key=lambda op: op.get("date"), reverse=new_list)
+    return sorted_data
